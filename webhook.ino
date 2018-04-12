838fa091aaf079a25372ecca47faeb9477095548
mehulwarade <mwarade@deakin.edu.au>
2018-04-13 00:21:56 +1000

    Update webhook.ino

--- webhook.ino
+++ webhook.ino
@@ -12,9 +12,9 @@ void setup() {
     String Randvalue = String(random(60, 80));
     int val = Function(Randvalue);
     Serial.printlnf("The random value id: %d" ,val);
-    delay(600);               // Wait for 60 seconds 
+    delay(60000);               // Wait for 60 seconds 
     digitalWrite(led, LOW);    // Turn OFF the LED 
-    delay(600);               // Wait for 60 seconds 
+    delay(60000);               // Wait for 60 seconds 
  } 
  
 int Function(String value){
